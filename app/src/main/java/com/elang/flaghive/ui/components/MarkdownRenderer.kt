6756c101e92e89924ab36674fafccbe7b08ed8f1
developer <developer@flaghive.app>
2026-06-24 13:53:30 +0700

    fix: correct MarkdownText import path

--- app/src/main/java/com/elang/flaghive/ui/components/MarkdownRenderer.kt
+++ app/src/main/java/com/elang/flaghive/ui/components/MarkdownRenderer.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
-import com.jeziellago.compose.markdown.MarkdownText
+import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
 fun MarkdownRenderer(
